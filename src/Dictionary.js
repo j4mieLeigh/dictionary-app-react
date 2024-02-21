b63ffa851a29bbe0f5f5c52d3ed547f17150f5a8
j4mieLeigh <149057654+j4mieLeigh@users.noreply.github.com>
2024-02-21 15:34:53 +0100

    Drafted Results, Meaning components

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,15 @@
 import React, { useState } from "react";
 import axios from "axios";
+import Results from "./Results";
 import "./Dictionary.css";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
+  let [results, setResults] = useState(null);
 
   function handleResponse(response) {
-    console.log(response.data[0]);
+    // response.data[0].meanings[0].definitions[0];
+    setResults(response.data[0]);
   }
 
   function search(event) {
@@ -33,6 +36,7 @@ export default function Dictionary() {
           value="Go"
         ></input>
       </form>
+      <Results results={results} />
     </div>
   );
 }
