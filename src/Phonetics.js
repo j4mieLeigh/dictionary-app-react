33126325ee8b2b88573dcfcd608ac4fc84db1956
j4mieLeigh <149057654+j4mieLeigh@users.noreply.github.com>
2024-02-22 15:09:39 +0100

    Drafted Phonetics component

---
+++ src/Phonetics.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Phonetics(props) {
+  return (
+    <div className="Phonetics">
+      <a
+        href={props.phonetics.audio}
+        target="_blank"
+      >
+        Listen
+      </a>
+      <br />
+      {props.phonetics.text}
+    </div>
+  );
+}
